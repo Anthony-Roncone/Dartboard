96ec2dde4d17e2d09dbfefb3272346329a7b477b
Anthony-Roncone <andoro524@gmail.com>
2023-02-11 17:52:10 -0800

    finished cricket gamemode

--- webapp/cricket.js
+++ webapp/cricket.js
@@ -1,11 +1,121 @@
-document.getElementById("score").focus();
+document.getElementById("0").focus();
+var id = 0;
+
+class scoreBox{
+    constructor(value, multiplyer, updated)
+    {
+        this.value = value;
+        this.multiplyer = multiplyer;
+        this.updated = updated;
+    }
+}
+
+var number = [];
+number.push(new scoreBox(0,1,false));
+number.push(new scoreBox(0,1,false));
+number.push(new scoreBox(0,1,false));
+number.push(new scoreBox(0,1,false));
+number.push(new scoreBox(0,1,false));
+number.push(new scoreBox(0,1,false));
+
+document.addEventListener('keydown', event => {
+    
+    if(event.shiftKey && event.key == "Enter")
+    {
+        changeFocus(2);  
+    }
+    else if(event.ctrlKey && event.key == "Enter")
+    {
+        changeFocus(3);
+    }
+    else if(event.altKey && event.key == "Enter")
+    {
+        clearInputs();
+    }
+    else if (event.key == 'Enter') 
+    {
+        changeFocus(1);
+    }
+});
+
+function changeFocus(mult)
+{
+    //console.warn("changeFocus");
+    if(number[id].updated == false)
+    {
+        var num = document.getElementById(id); 
+        number[id].value = num.value;
+        number[id].multiplyer = mult;
+        number[id].updated = true;
+        num.value += " *" + mult;
+        updateTempScore();
+    }
+    id = (id + 1) % 6;
+    document.getElementById(id).focus();
+}
+
+function updateTempScore()
+{
+    var scores = document.getElementsByName(number[id].value);
+    if(scores.length > 0)
+    {
+        var player;
+        if(id <= 2)
+            player = 0;
+        else if(id >= 3)
+            player = 1;
+
+        for(var i=0; i< number[id].multiplyer; i++)
+            if(!numberFinished(scores[player].innerHTML))
+                scores[player].innerHTML += "X";
+
+        if(checkVictory(player))
+        {   
+            var winner = document.getElementsByName("winner");
+            winner[player].style.display = 'inline';
+            disableInput();
+            //alert("Player " + (player + 1) + " Wins!");
+        }
+            
+    }
+}
+
+function disableInput()
+{
+    for(var i=0; i<6; i++)
+    {
+        document.getElementById(i).disabled = true;
+    }
+}
+
+function clearInputs()
+{
+    //console.warn("clearInputs");
+    document.getElementById("0").value = "";
+    document.getElementById("1").value = "";
+    document.getElementById("2").value = "";
+    document.getElementById("3").value = "";
+    document.getElementById("4").value = "";
+    document.getElementById("5").value = "";
+
+    for(var i=0; i<6; i++)
+    {
+        number[i].value = 0;
+        number[i].multiplyer = 1;
+        number[i].updated = false;
+    }
+
+    id = 0;
+    document.getElementById(id).focus();
+}
 
 function updateScore(player)
 {
-    var name = document.getElementById("score").value;
+    var name = document.getElementById(id).value;
     var scores = document.getElementsByName(name);
-    if(!numberFinished(scores[player].innerHTML))
-        scores[player].innerHTML += "X";
+    for(var i=0; i< number[id].multiplyer; i++)
+        if(!numberFinished(scores[player].innerHTML))
+            scores[player].innerHTML += "X";
     if(checkVictory(player))
         alert("Player " + (player + 1) + " Wins!");
 }
@@ -32,5 +142,5 @@ function checkVictory(player)
 
 function setFocus()
 {
-    document.getElementById("score").focus();
+    document.getElementById(id).focus();
 }
